2954d812d3ea711ec6edc06851bfca5b68957a02
hafizhghiyats29 <hafizhghiyats29@gmail.com>
2023-03-06 00:33:28 +0700

    edit modal

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-const CheckBox = React.forwardRef( ({indeterminate, ...rest}, ref) => {
+import React from "react";
+const CheckBox = React.forwardRef(({ indeterminate, ...rest }, ref) => {
   const defaultRef = React.useRef();
   const resolvedRef = ref || defaultRef;
 
   return (
     <>
-      <input type="checkbox" ref={resolvedRef} {...rest} className="w-[16px] h-[16px] bg-[#2D5BFF]"/>
+      <input type="checkbox" ref={resolvedRef} {...rest} className="w-[18px] h-[18px] border-[#2D5BFF] bg-[#2D5BFF]" />
     </>
-
-  )
-})
+  );
+});
 export default CheckBox;
