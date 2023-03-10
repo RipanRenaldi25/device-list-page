338d9d7ac392ac0b4a575d38ae93fe708c2d1f1d
hafizhghiyats29 <hafizhghiyats29@gmail.com>
2023-03-10 11:17:46 +0700

    re commit

--- src/components/FormPreyen.jsx
+++ src/components/FormPreyen.jsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormPreyen = () => {
+  const [inputValue, setInputValue] = useState({});
+  function onChangeInputHandler({ target }) {
+    const { name, value } = target;
+    setInputValue((prevState) => ({ ...prevState, [name]: value }));
+    console.log(inputValue);
+  }
   return (
-    <div>
+    <form>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">SIP Number</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="sip" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">Device Name</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="device" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">ALIAS</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="alias" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">Ip Address</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="ipaddress" />
       </div>
-    </div>
+    </form>
   );
 };
 
--- src/components/FormRadio.jsx
+++ src/components/FormRadio.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormRadio = () => {
+  const [inputValue, setInputValue] = useState({});
+  function onChangeInputHandler({ target }) {
+    const { name, value } = target;
+    setInputValue((prevState) => ({ ...prevState, [name]: value }));
+    console.log(inputValue);
+  }
   return (
     <div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">SIP Number</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="sip" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">Device Name</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="device" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">ALIAS</span>
         <span>:</span>
-        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" />
+        <input className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4] px-2 py-1" onChange={onChangeInputHandler} name="alias" />
       </div>
       <div className="flex items-center mb-4 text-sm">
         <span className="basis-44">Device Type</span>
         <span>:</span>
-        <select className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4]">
+        <select className="w-[220px] ml-[18px] border-2 p-1.5 rounded-md border=[#D4D4D4]" onChange={onChangeInputHandler} name="type">
           <option></option>
           <option value="ff">Full Function (FF)</option>
           <option value="radio">Low Function (LF)</option>
